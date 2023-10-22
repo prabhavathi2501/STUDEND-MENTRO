eeefb67875a3e14d0ff22129f1b32396c62f3327
prabhavathi2501 <prabhashok2501@gmail.com>
2023-10-22 18:51:26 +0530

    updated

--- src/models/user.js
+++ src/models/user.js
@@ -3,13 +3,7 @@ const mongoose = require('./index')
 
 
 const userSchema = new mongoose.Schema({
-    // firstName:{type:String,required:[true,"First Name is required"]},
-    // lastName:{type:String,required:[true,"Last Name is required"]},
-    // email:{type:String,required:[true,"Email is required"]},
-    // password:{type:String,required:[true,"Password is required"]},
-    // status:{type:Boolean,default:true},
-    // role:{type:String,default:'customer'},
-    // createdAt:{type:Date, default:Date.now()}
+    
     firstName:String,
     lastName:String,
     email:String,
